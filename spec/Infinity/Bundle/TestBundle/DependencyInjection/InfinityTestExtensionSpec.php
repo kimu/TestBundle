df4ceb74b659c21712fbe6abdb88ba14b6ccd433
kimu <patrick.polloni@gmail.com>
2014-03-05 10:28:54 +0000

    [TestBundle] Improves comments and name of test functions

--- spec/Infinity/Bundle/TestBundle/DependencyInjection/InfinityTestExtensionSpec.php
+++ spec/Infinity/Bundle/TestBundle/DependencyInjection/InfinityTestExtensionSpec.php
@@ -13,11 +13,11 @@ class InfinityTestExtensionSpec extends ObjectBehavior
         $this->shouldHaveType('Infinity\Bundle\TestBundle\DependencyInjection\InfinityTestExtension');
     }
 
-    function it_should_assign_substitutions_to_the_container_if_enabled_and_substitutions_key_is_in_the_config(ContainerBuilder $containerBuilder)
+    function it_should_assign_substitutions_to_the_container_and_load_services_if_enabled_and_substitutions_key_is_in_the_config(ContainerBuilder $containerBuilder)
     {
         $configs = [['enabled' => true, 'substitutions' => []]];
 
-        // Called when the yaml loader is initialised
+        // Called when services are loaded
         $containerBuilder->addResource(Argument::any())->shouldBeCalled();
 
         // Assign the substitutions array to the container
@@ -26,12 +26,12 @@ class InfinityTestExtensionSpec extends ObjectBehavior
         $this->load($configs, $containerBuilder);
     }
 
-    function it_should_assign_substitutions_to_the_container_if_enabled_and_substitutions_key_is_not_in_the_config(ContainerBuilder $containerBuilder)
+    function it_should_assign_substitutions_to_the_container_and_load_services_if_enabled_and_substitutions_key_is_not_in_the_config(ContainerBuilder $containerBuilder)
     {
         // Subsitutions is added by the tree by default as an empty array
         $configs = [['enabled' => true]];
 
-        // Called when the yaml loader is initialised
+        // Called when services are loaded
         $containerBuilder->addResource(Argument::any())->shouldBeCalled();
 
         // Assign the substitutions array to the container
@@ -40,13 +40,15 @@ class InfinityTestExtensionSpec extends ObjectBehavior
         $this->load($configs, $containerBuilder);
     }
 
-    function it_should_not_assign_substitutions_to_the_container_if_not_enabled(ContainerBuilder $containerBuilder)
+    function it_should_not_assign_substitutions_to_the_container_and_not_load_services_if_not_enabled(ContainerBuilder $containerBuilder)
     {
         // Enabled is false by default
         $configs = [];
 
+        // Do not load services
         $containerBuilder->addResource(Argument::any())->shouldNotBeCalled();
 
+        // Do not assign substitution to the container
         $containerBuilder->setParameter('infinity_test.substitutions', [])->shouldNotBeCalled();
 
         $this->load($configs, $containerBuilder);
