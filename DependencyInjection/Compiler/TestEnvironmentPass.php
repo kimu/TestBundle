d185297efeea5ad27666ee75cc710d5a1d78a503
kimu <patrick.polloni@gmail.com>
2014-03-05 15:08:21 +0000

    [TestBundle] Improves logic of TestEnvironmentPass

--- DependencyInjection/Compiler/TestEnvironmentPass.php
+++ DependencyInjection/Compiler/TestEnvironmentPass.php
@@ -13,27 +13,24 @@ class TestEnvironmentPass implements CompilerPassInterface
 {
     public function process(ContainerBuilder $container)
     {
-        // This compiler pass acts only in the test environment
-        if ($container->getParameter('kernel.environment') == 'test') {
-            // Checks if substitutions has been configured and proceeds with the substitution
-            if ($container->has('infinity_test.substitutions')) {
-                foreach ($container->get('infinity_test.substitutions') as $substitution) {
-                    // Extracts the name of the service
-                    $service = array_keys($substitution)[0];
-                    if ($container->hasDefinition($service)) {
-                        $definition = $container->getDefinition($service);
+        // This compiler pass acts only in the test environment and if substitutions has been configured
+        if ($container->getParameter('kernel.environment') == 'test' && $container->has('infinity_test.substitutions')) {
+            foreach ($container->get('infinity_test.substitutions') as $substitution) {
+                // Extracts the name of the service
+                $service = array_keys($substitution)[0];
+                if ($container->hasDefinition($service)) {
+                    $definition = $container->getDefinition($service);
 
-                        // Normalize the class name
-                        $class = preg_replace('/(\/\/|\/|\\\)/', '\\\\', $substitution[$service]['class']);
-                        $definition->setClass($class);
+                    // Normalize the class name
+                    $class = preg_replace('/(\/\/|\/|\\\)/', '\\\\', $substitution[$service]['class']);
+                    $definition->setClass($class);
 
-                        // If requested remove all arguments
-                        if (false === $substitution[$service]['inherit_arguments']) {
-                            $definition->setArguments([]);
-                        }
-
-                        $container->setDefinition($service, $definition);
+                    // If requested remove all arguments
+                    if (false === $substitution[$service]['inherit_arguments']) {
+                        $definition->setArguments([]);
                     }
+
+                    $container->setDefinition($service, $definition);
                 }
             }
         }
