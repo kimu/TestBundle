02db91a3a2329f711c7551e1e90e62b4c118e191
kimu <patrick.polloni@gmail.com>
2014-03-20 11:11:13 +0000

    [TestBundle] Adds getScreenshotOnfailure mathod @AfterStep callback

--- Test/Context/FeatureContext.php
+++ Test/Context/FeatureContext.php
@@ -2,10 +2,12 @@
 
 use Behat\Behat\Exception\PendingException;
 use Behat\Gherkin\Node\PyStringNode,
-    Behat\Gherkin\Node\TableNode;
+    Behat\Gherkin\Node\TableNode,
+    Behat\Behat\Event\StepEvent;
 use Behat\MinkExtension\Context\MinkContext,
     Behat\MinkExtension\Context\RawMinkContext;
 use Symfony\Component\Finder\Finder;
+
 //
 // Require 3rd-party libraries here:
 //
@@ -35,6 +37,43 @@ class FeatureContext extends RawMinkContext
             if (__CLASS__ != $class = $file->getBasename('.php')) {
                 $this->useContext($class, new $class($parameters));
             }
+        }
+    }
+
+    /**
+     * @AfterStep
+     *
+     * @param StepEvent $event
+     */
+    public function getScreenshotOnFailure(StepEvent $event)
+    {
+        // 4 is the exit code for failing steps && We can get a screenshot only if Selenium2 is used
+        if (4 === $event->getResult() && $this->getSession()->getDriver() instanceof \Behat\Mink\Driver\Selenium2Driver) {
+            $screenshot = $this->getSession()->getDriver()->getScreenshot();
+            $file       = sys_get_temp_dir().'/firefox_'.date('Ymd_His').'.png';
+            file_put_contents($file, $screenshot);
+
+            $body = 'Screenshot of failing step: '.$event->getStep()->getText();
+
+            if (false !== getenv('TRAVIS')) {
+                $body .= PHP_EOL.
+                    'Branch: '.getenv('TRAVIS_BRANCH').PHP_EOL.
+                    'Commit: '.getenv('TRAVIS_COMMIT').PHP_EOL.
+                    'Pull Request: '.getenv('TRAVIS_PULL_REQUEST').PHP_EOL.
+                    'User/Repo: '.getenv('TRAVIS_REPO_SLUG')
+                ;
+            }
+
+            // Send the email with the screenshot attached
+            $message = \Swift_Message::newInstance()
+                ->setSubject('Failing step: '.$event->getStep()->getText())
+                ->setFrom('server@'.gethostname())
+                ->setTo(['patrick.polloni@infinitycloud.com', 'chris.sedlmayr@infinitycloud.com', 'david.north@infinitycloud.com'])
+                ->setBody($body)
+                ->attach(\Swift_Attachment::fromPath($file))
+            ;
+
+            \Swift_Mailer::newInstance(\Swift_MailTransport::newInstance())->send($message);
         }
     }
 }
